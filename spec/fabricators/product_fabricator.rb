4dd67cd6ef1ffa90df9cb45a9239c81de8638472
Mateo mojica <mmojicah.84@gmail.com>
2022-10-05 21:00:07 -0500

    Add fabricator for the product model

---
+++ spec/fabricators/product_fabricator.rb
@@ -0,0 +1,9 @@
+require 'faker'
+
+Fabricator(:product) do
+  # category
+  name: {Faker::Commerce.product_name}
+  quantity: {Faker::Number.between(from: 1, to: 100)}
+  description: {Faker::Lorem.sentence}
+  image_url: {Faker::Avatar.image}
+end
