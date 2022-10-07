20491ef64b1503d9395d13302770b8c706266314
Mateo mojica <mmojicah.84@gmail.com>
2022-10-06 21:50:22 -0500

    Create specs for price model

--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Price, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:price) { Fabricate(:price) }
+
+  context 'Price Model' do
+    describe 'Associations' do
+      it 'should have one product associated' do
+        expect(price.product).not_to be_nil
+      end
+
+      it 'should have one store associated' do
+        expect(price.store).not_to be_nil
+      end
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,9 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+# require 'action_cable/testing/rspec'
+# require 'test_prof/recipes/rspec/before_all'
+# require 'test_prof/recipes/rspec/let_it_be'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
