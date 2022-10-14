abf40e0970534f8ab7b40cac14ed7df8e60ce23d
Mateo mojica <mmojicah.84@gmail.com>
2022-10-13 20:52:58 -0500

    Update routes for the product controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -6,4 +6,63 @@ class ProductController < ApplicationController
     })
     render json: response, status: 200
   end
+
+  def get_single_product
+    begin
+      record = Product.find(get_product_id_param)
+      response = ActiveSupport::HashWithIndifferentAccess.new({
+        product: record
+      })
+      render json: response, status: 200
+    rescue => e
+      render json: { error: e }, status: 400
+    end
+  end
+  
+  def create_product
+    ActiveRecord::Base.transaction do
+      begin
+        record = Product.create!(get_product_params)
+        render json: { status: 'created', product_id: record.id}, status: 201
+      rescue => e
+        render json: e, status: 400
+      end
+    end
+  end
+
+  def update_product
+    ActiveRecord::Base.transaction do
+      begin
+        record = Product.find(get_product_id_param)
+        record.update!(get_product_params)
+
+        render json: { status: 'updated', product_id: record.id}, status: 201
+      rescue => e
+        render json: { error: e}, status: 400
+      end
+    end
+  end
+
+  def remove_product
+    ActiveRecord::Base.transaction do
+      begin
+        record = Product.find(get_product_id_param)
+        record.delete
+
+        render json: { status: 'deleted'}, status: 201
+      rescue => e
+        render json: {error: e}, status: 400
+      end
+    end
+  end
+
+  private
+
+  def get_product_id_param
+    params.permit(:id)[:id]
+  end
+
+  def get_product_params
+    params.require(:product).permit(:name, :quantity, :description, :image_url, :category_id)
+  end
 end
--- config/routes/products.rb
+++ config/routes/products.rb
@@ -1,3 +1,7 @@
 scope "/products" do
   get '/all', to: 'product#get_all_products'
+  get '/:id', to: 'product#get_single_product'
+  post '/create', to: 'product#create_product'
+  put '/:id', to: 'product#update_product'
+  delete '/:id', to: 'product#remove_product'
 end
