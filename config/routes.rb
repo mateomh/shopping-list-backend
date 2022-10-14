8f0d531a8926baaec3c1d1831b700c5d11c3f6a8
Mateo mojica <mmojicah.84@gmail.com>
2022-10-13 20:52:28 -0500

    Update the routes for the new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,24 @@ Rails.application.routes.draw do
   put 'categories/:id', to: 'category#update_category'
   delete 'categories/:id', to: 'category#remove_category'
 
+  # Store model routes
+  scope :stores, defaults: { format: 'json' } do
+    get '/all', to: 'store#get_all_stores'
+    get '/:id', to: 'store#get_single_store'
+    post '/create', to: 'store#create_store'
+    put '/:id', to: 'store#update_store'
+    delete '/:id', to: 'store#remove_store'
+  end
+
+  # Price model routes
+  scope :prices, defaults: { format: 'json' } do
+    get '/all', to: 'price#get_all_prices'
+    get '/:id', to: 'price#get_single_price'
+    post '/create', to: 'price#create_price'
+    put '/:id', to: 'price#update_price'
+    delete '/:id', to: 'price#remove_price'
+  end
+
   # Routes through files
   def get_file_routes(name)
     instance_eval(File.read(Rails.root.join("config/routes/#{name}.rb")))
