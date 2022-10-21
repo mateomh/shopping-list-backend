89d53b59d60b1b705bfe33a4b074407ef4b30749
Mateo mojica <mmojicah.84@gmail.com>
2022-10-20 20:58:38 -0500

    Update category controller to use the created services

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,40 +1,55 @@
 class CategoryController < ApplicationController
   def get_all_categories
-    categories = Category.all
-    render json: {
-      categories: categories
-    }
+    begin
+      result = Categories::GetAllCategories.run!
+      render json: {
+        categories: result
+      },  status: 200
+    rescue => e
+      render json: { Error: e }, status: 400
+    end
   end
 
   def get_single_category
     begin
-      record = Category.find(get_category_id_param)
-      response = ActiveSupport::HashWithIndifferentAccess.new({
-        category: record
-      })
-      render json: response, status: 200
+      # record = Category.find(get_category_id_param)
+      # response = ActiveSupport::HashWithIndifferentAccess.new({
+      #   category: record
+      # })
+      result = Categories::GetCategoryInfo.run!(
+        category_id: get_category_id_param
+      )
+
+      render json: result, status: 200
     rescue => e
       render json: { error: e }, status: 400
     end
   end
 
   def create_category
-    ActiveRecord::Base.transaction do
-      begin
-        record = Category.create!(get_category_params)
-        render json: { status: 'created', cartegory_id: record.id}, status: 201
-      rescue => e
-        render json: e, status: 400
-      end
+    begin
+      # record = Category.create!(get_category_params)
+      result = Categories::CreateCategory.run!(
+        name: get_category_params[:name],
+        description: get_category_params[:description]
+      )
+
+      render json: { status: 'created', cartegory_id: result.id}, status: 201
+    rescue => e
+      render json: e, status: 400
     end
   end
 
   def update_category
     begin
-      record = Category.find(get_category_id_param)
-      record.update!(get_category_params)
+      # record = Category.find(get_category_id_param)
+      # record.update!(get_category_params)
+      result = Categories::UpdateCategory.run!(
+        category_id: get_category_id_param,
+        category_information: get_category_params
+      )
 
-      render json: { status: 'updated', cartegory_id: record.id}, status: 201
+      render json: { status: 'updated', cartegory_id: result.id}, status: 201
     rescue => e
       render json: { params: get_category_params, error: e}, status: 400
     end
@@ -42,8 +57,11 @@ class CategoryController < ApplicationController
 
   def remove_category
     begin
-      record = Category.find(get_category_id_param)
-      record.delete
+      # record = Category.find(get_category_id_param)
+      # record.delete
+      Categories::DeleteCategory.run!(
+        category_id: get_category_id_param
+      )
 
       render json: { status: 'deleted'}, status: 201
     rescue => e
