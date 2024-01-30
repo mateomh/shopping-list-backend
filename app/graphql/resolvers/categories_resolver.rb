f2c0d2de2ccb581b847189c894c9571be7825b28
Mateo mojica <mmojicah.84@gmail.com>
2024-01-29 21:26:12 -0500

    Add description to the categories resolver

--- app/graphql/resolvers/categories_resolver.rb
+++ app/graphql/resolvers/categories_resolver.rb
@@ -1,5 +1,6 @@
 module Resolvers
   class CategoriesResolver < GraphQL::Schema::Resolver
+    description "Returns all avaliable categories"
     type [Types::CategoryType], null: false
 
     def resolve
